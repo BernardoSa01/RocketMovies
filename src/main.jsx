a93f09d37610f80cb927a3b8e244123ad9246013
bernardo <bernardo_nf@hotmail.com>
2024-09-23 18:15:40 -0300

    #feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,15 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Home } from './pages/Home/index'
+import { Routes } from './routes'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Routes />
     </ThemeProvider>
   </StrictMode>,
 )
